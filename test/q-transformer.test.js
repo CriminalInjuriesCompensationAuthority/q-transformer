b7e7f1a6f75b327bd081aaea7b04b9622bbc3cd6
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2019-08-27 10:38:17 +0100

    PR requested changes

--- test/q-transformer.test.js
+++ test/q-transformer.test.js
@@ -2847,6 +2847,7 @@ describe('qTransformer', () => {
                     for (const answer in formattedAnswer) {
                         // eslint-disable-next-line no-prototype-builtins
                         if (formattedAnswer.hasOwnProperty(answer)) {
+                            console.log(answer);
                             expect(Object.keys(formattedAnswer[answer])).toContain('value');
                             expect(Object.keys(formattedAnswer[answer])).toContain('href');
                         }
