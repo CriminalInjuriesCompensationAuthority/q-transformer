ea9ed461ba8742729bb81c67e204647f705efefa
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2019-08-27 16:21:32 +0100

    Fix govukSelect transformer

--- test/q-transformer.test.js
+++ test/q-transformer.test.js
@@ -731,6 +731,10 @@ describe('qTransformer', () => {
                             hint: null,
                             items: [
                                 {
+                                    text: 'Please select...',
+                                    value: ''
+                                },
+                                {
                                     value: 'england',
                                     text: 'England'
                                 },
@@ -2558,6 +2562,10 @@ describe('qTransformer', () => {
                         text: 'Please select an option'
                     },
                     items: [
+                        {
+                            text: 'Please select...',
+                            value: ''
+                        },
                         {
                             value: 'england',
                             text: 'England'
