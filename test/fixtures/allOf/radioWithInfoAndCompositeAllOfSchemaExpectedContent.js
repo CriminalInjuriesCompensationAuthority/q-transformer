344d92d3d774a602414a493007b965fd05b2f6c3
neil-stephen-mcgonigle <Neil.McGonigle@CICA.GOV.UK>
2021-12-09 08:56:21 +0000

    expected test data updated with new line formatting

--- test/fixtures/allOf/radioWithInfoAndCompositeAllOfSchemaExpectedContent.js
+++ test/fixtures/allOf/radioWithInfoAndCompositeAllOfSchemaExpectedContent.js
@@ -10,13 +10,25 @@ module.exports = {
         "name": "q-applicant-has-crime-reference-number",
         "fieldset": {
             "legend": {
-                "text": "Do you have a crime reference number?"                
-        }
+                "text": "Do you have a crime reference number?"
+            }
     },
     "hint": {
-        "text": "This is the number the police gave the crime when it was reported. We need this to get information about the crime from them. You will need to add this later in the application."},"items": [{"value": true,"text": "Yes"},{"value": false,"text": "No"}],"classes": "govuk-radios--inline"}) }}{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "I do not know the crime reference number",html: '<p class="govuk-body">If you do not have your crime reference number, call 101 to speak to your local police station. They can help you get this.</p>'
-        })
-    }}
+        "text": "This is the number the police gave the crime when it was reported. We need this to get information about the crime from them. You will need to add this later in the application."
+    },
+    "items": [
+        {
+            "value": true,
+            "text": "Yes"
+    },
+    {
+        "value": false,
+        "text": "No"
+    }
+],
+"classes": "govuk-radios--inline"
+}) }}
+{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "I do not know the crime reference number",html: '<p class="govuk-body">If you do not have your crime reference number, call 101 to speak to your local police station. They can help you get this.</p>'})}}
     {% from "fieldset/macro.njk" import govukFieldset %}
     {% call govukFieldset({
         legend: {
