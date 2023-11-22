25820eab7f73e9e6a527c3c3a2746c6593c3b781
tjbburton <Thomas.Burton@CICA.gov.uk>
2023-11-22 08:19:41 +0000

    Update linting to be consistent with cica web

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,26 @@
+'use strict';
+
 /*! m0-start */
 const config = {
     extends: ['airbnb-base', 'prettier', 'plugin:jest/recommended'],
     env: {
         node: true
     },
+    // overwrite airbnb-base to use commonjs instead of ES6 modules
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'script',
+        ecmaFeatures: {
+            modules: false
+        }
+    },
     rules: {
         'prettier/prettier': ['error'],
         'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
         curly: ['error', 'all'],
         'jest/expect-expect': ['error'],
+        // https://github.com/eslint/eslint/issues/8953#issuecomment-317697474
+        strict: ['error', 'safe'],
         'no-param-reassign': ['error', {props: false}]
     },
     plugins: ['prettier']
