5c6d6cad5a0eccbc29625b64512e852428a31ea3
tjbburton <Thomas.Burton@CICA.gov.uk>
2023-11-24 09:42:12 +0000

    Update CYA URls in tests to info- format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*! m0-start */
 const config = {
     extends: ['airbnb-base', 'prettier', 'plugin:jest/recommended'],
@@ -19,8 +17,6 @@ const config = {
         'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
         curly: ['error', 'all'],
         'jest/expect-expect': ['error'],
-        // https://github.com/eslint/eslint/issues/8953#issuecomment-317697474
-        strict: ['error', 'safe'],
         'no-param-reassign': ['error', {props: false}]
     },
     plugins: ['prettier']
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*! m0-start */
 const config = {
     testEnvironment: 'node'
